1cb3a0147d4a4783bd38aef0030693fd6ba6b1b4
RA2411026010741 <ss8319@srmist.edu.in>
2026-03-12 02:04:23 +0530

    Create UseCase7PalindromeCheckerApp.java

---
+++ src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,42 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Deque<Character> deque = new LinkedList<>();
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Insert characters into deque
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and rear elements
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println("The string is a Palindrome.");
+        } else {
+            System.out.println("The string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
